145d377d2d3af97e5bee928fb99f820f408144e0
Nazarii Tkach <113298067+NazariiTkach@users.noreply.github.com>
2023-03-05 15:33:29 +0100

    index.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -15,3 +15,21 @@ export const fetchCountries = name => {
       console.error(error);
     });
 };
+
+// export const fetchCountries = name => {
+//     return fetch(
+//         `https://restcountries.com/v3.1/name/${name}?fields=,name.official,capital,population,flags.svg,languages`
+//     )
+//         .then(response => {
+//             if (!response.ok) {
+//                 if (response.status === 404) {
+//                     return [];
+//                 }
+//                 throw new Error(response.status);
+//             }
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.error(error);
+//     })
+// }
